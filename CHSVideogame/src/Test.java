c0067d6dee01a0ef6bb5e39843930ad004b8eded
MichaelMorton1 <113527212+MichaelMorton1@users.noreply.github.com>
2023-02-13 14:45:33 +0000

    added "good morning"

--- CHSVideogame/src/Test.java
+++ CHSVideogame/src/Test.java
@@ -1,9 +1,8 @@
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello World.");
 		System.out.println("Goodbye World");
+		System.out.println("Good morning");
 	}
-
 }
