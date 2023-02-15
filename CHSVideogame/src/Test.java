b792a3d114ff1efe684c43b96389cdb35ef17efd
dia <dia.nawathe@gmail.com>
2023-02-15 16:54:24 -0500

    deleted a line

--- CHSVideogame/src/Test.java
+++ CHSVideogame/src/Test.java
@@ -1,7 +1,6 @@
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello World.");
 		System.out.println("Goodbye World");
 		System.out.println("AAAAAAAAAAAAAAAAA");
