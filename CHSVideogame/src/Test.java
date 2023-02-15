87e34322fb992a391397df650680eaa109f7cd7f
Alexander <alexanderrauch27@gmail.com>
2023-02-15 09:37:39 -0500

    dia

--- CHSVideogame/src/Test.java
+++ CHSVideogame/src/Test.java
@@ -1,10 +1,11 @@
 public class Test {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World.");
 		System.out.println("Goodbye World");
 		System.out.println("ARR");
+		// git add .
+		// git commit -m "Explain"
+		// git push  
 	}
-
 }
