1b3c94a86eab3e50507e4f4ce6a7776accc4db0c
Lethargic Logistics <61340443+LogisticsAndLaziness@users.noreply.github.com>
2023-02-15 09:48:33 -0500

    Game class initial changes

--- CHSVideogame/src/Game.java
+++ CHSVideogame/src/Game.java
@@ -1,9 +1,18 @@
+import java.util.ArrayList;
 public class Game {
     static enum Difficulty{
         EASY,
         MEDIUM,
         HARD
     }
+    private Map map;
+    private Camera camera;
+    private ArrayList<InteractableObject> objects;
+    private ArrayList<OncommingStudent> students;
+    private Player player;
+    private int score;
+    
+
     public Game(){
         System.out.println("More tests");
     }
