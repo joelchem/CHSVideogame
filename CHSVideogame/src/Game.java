291b2ed28ff6893f647116d34a63ffcd75b78305
jellyfishyishy <102619753+jellyfishyishy@users.noreply.github.com>
2023-02-16 10:10:55 -0500

    game class, assets folder

--- CHSVideogame/src/Game.java
+++ CHSVideogame/src/Game.java
@@ -1,5 +1,28 @@
+import java.io.*;
+import java.util.*;
+
 public class Game {
+	private Map map;
+	private Camera camera;
+	private ArrayList<InteractableObject> interactableObjects;
+	private ArrayList<OncomingStudent> oncomingStudents;
+	private Player player;
+	private int score;
+	private File scoreBoard;
+	private double remainingDistance;
+	private String difficulty;
+	
     public Game(){
-        System.out.println("More tests");
+        camera = new Camera(this);
+        oncomingStudents = new ArrayList<>();
+        interactableObjects = new ArrayList<>();
+        score = 0;
+        player = new Player(this);
+        scoreBoard = new File("\\CHSVideogame\\assets\\scoreBoard.txt");
+        try {
+        	scoreBoard.createNewFile();
+        } catch(Exception ex) {
+        	System.out.println("LET ME IN");
+        }
     }
 }
