adc1dd94d28f00d9ae2c25c5f01093975983d5b7
Lethargic Logistics <61340443+LogisticsAndLaziness@users.noreply.github.com>
2023-02-16 10:03:13 -0500

    Game, GameRunner classes
    First game, gamerunner class commits

---
+++ CHSVideogame/src/GameRunner.java
@@ -0,0 +1,26 @@
+public class GameRunner {
+    Game game;
+    Map map;
+    public GameRunner(Game game, Map map){
+        this.game = game;
+        this.map = map;
+    }
+    public void startGameloop(){
+        //might need to re-explain how calculateFrame interacts within the method here
+        calculateFrame();
+        //x is a placeholder for value based on difficulty 
+        int x = 0;
+        game.setMap(new Map());
+        game.setPlayer(new Player());
+        game.setDifficulty(map.getDifficulty());
+
+        for(int i = 0; i < x; i++){
+            game.addInteractableObject(new InteractableObject());
+            game.addOncomingStudent(new OncomingStudent());
+        }
+
+    }
+
+    private void calculateFrame(){
+    }
+}
