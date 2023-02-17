9f1f23783c090eecbfc20dc6a5fc4706d838eaa3
jellyfishyishy <102619753+jellyfishyishy@users.noreply.github.com>
2023-02-17 09:43:49 -0500

    BEN

--- CHSVideogame/src/Game.java
+++ CHSVideogame/src/Game.java
@@ -4,8 +4,8 @@ import java.util.*;
 public class Game {
 	private Map map;
 	private Camera camera;
-	private ArrayList<InteractableObject> interactableObjects;
 	private ArrayList<OncomingStudent> oncomingStudents;
+	private ArrayList<DisplayObject> displayObjects;
 	private Player player;
 	private int score;
 	private File scoreBoard;
@@ -13,16 +13,37 @@ public class Game {
 	private String difficulty;
 	
     public Game(){
+    	map = new Map(this);
         camera = new Camera(this);
         oncomingStudents = new ArrayList<>();
-        interactableObjects = new ArrayList<>();
-        score = 0;
+        displayObjects = new ArrayList<>();
         player = new Player(this);
+        score = 0;
         scoreBoard = new File("\\CHSVideogame\\assets\\scoreBoard.txt");
         try {
         	scoreBoard.createNewFile();
         } catch(Exception ex) {
         	System.out.println("LET ME IN");
         }
+    }
+    
+    public DisplayObject getDisplayObject(int index) {
+    	return displayObjects.get(index);
+    }
+    
+    public void addDisplayObject (DisplayObject obj) {
+    	displayObjects.add(obj);
+    }
+    
+    public void removeDisplayObject(int index) {
+    	displayObjects.remove(index);
+    }
+   
+    public OncomingStudents getOncomingStudents(int index) {
+    	return oncomingStudents.get(index);
+    }
+    
+    public void addOncomingStudent(OncomingStudent student) {
+    	oncomingStudents.add(student);
     }
 }
