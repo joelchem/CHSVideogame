3b3a90ab514c7f5b40d47b101a105b3d76a9d4fe
jellyfishyishy <102619753+jellyfishyishy@users.noreply.github.com>
2023-02-17 09:52:58 -0500

    copied in Game class from BEN

--- CHSVideogame/src/Game.java
+++ CHSVideogame/src/Game.java
@@ -1,11 +1,49 @@
+import java.io.*;
+import java.util.*;
 
 public class Game {
+	private Map map;
+	private Camera camera;
+	private ArrayList<OncomingStudent> oncomingStudents;
+	private ArrayList<DisplayObject> displayObjects;
+	private Player player;
+	private int score;
+	private File scoreBoard;
+	private double remainingDistance;
+	private String difficulty;
 	
-	public Game(){
-        System.out.println("More tests");
+    public Game(){
+    	map = new Map(this);
+        camera = new Camera(this);
+        oncomingStudents = new ArrayList<>();
+        displayObjects = new ArrayList<>();
+        player = new Player(this);
+        score = 0;
+        scoreBoard = new File("\\CHSVideogame\\assets\\scoreBoard.txt");
+        try {
+        	scoreBoard.createNewFile();
+        } catch(Exception ex) {
+        	System.out.println("LET ME IN");
+        }
     }
-	public static void main(String[] args) {
-		System.out.println("-------");
-	}
-	
+    
+    public DisplayObject getDisplayObject(int index) {
+    	return displayObjects.get(index);
+    }
+    
+    public void addDisplayObject (DisplayObject obj) {
+    	displayObjects.add(obj);
+    }
+    
+    public void removeDisplayObject(int index) {
+    	displayObjects.remove(index);
+    }
+   
+    public OncomingStudents getOncomingStudents(int index) {
+    	return oncomingStudents.get(index);
+    }
+    
+    public void addOncomingStudent(OncomingStudent student) {
+    	oncomingStudents.add(student);
+    }
 }
