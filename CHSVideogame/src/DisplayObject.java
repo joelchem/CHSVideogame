a70c72fa4b8d6442fc0caa15257295b65b3bf3c9
MichaelMorton1 <ipibym@gmail.com>
2023-02-21 11:38:06 -0500

    Added some logic to DisplayObject

--- CHSVideogame/src/DisplayObject.java
+++ CHSVideogame/src/DisplayObject.java
@@ -22,16 +22,42 @@ public class DisplayObject {
         return dimensions[1];
     }
 
+    
     public void testForCollision() {
-
+    	Player player = game.getPlayer();
+    	//First check to see if the sprite is left or right of the player
+    	//Then check up and down
+    	
+    	int playerPosX = player.getPositionX();
+    	int playerPosY = player.getPositionY();
+    	
+    	//Call overlap with x and y values, if it passes, call onCollision
+    	//NOTE: May need to modify the dimension adding in the future
+    	
+    	boolean overlapX = overLap(posX, posX+dimensions[0], player.getPositionX(), player.getPositionX()+)
+    	
+    	if (overLap(posX, posX+dimensions[0], )) {
+    		onCollision();
+    	}
+    	
+    	
+    }
+    
+    private boolean overLap(int spriteStart, int spriteEnd, int playerStart, int playerEnd) {
+    	
+    	return false;
     }
 
     public void renderSprite() {
-
+    	
     }
 
     //To be specialized, used for determining conditions in the event that the player collides with a given interactable object
     public void onCollision() {
+    	int xCoord = 0;
+    	int yCoord = 0;
+    	
+    	
 
     }
 
