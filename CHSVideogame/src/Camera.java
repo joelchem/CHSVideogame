f0796c010642f3b429f5e2de07aa519ebc5d012c
teky1 <joelchem@outlook.com>
2023-02-21 10:09:34 -0500

    worked on cam stuff

--- CHSVideogame/src/Camera.java
+++ CHSVideogame/src/Camera.java
@@ -1,16 +1,41 @@
+import java.awt.Point;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Point2D;
 
 public class Camera {
 	
-	private int positionX;
-	private int positionY;
+	private Point position;
 	private double heading;
-	private Game game;
+//	private Game game;
 	
-	Camera(Game gameObj) {
-		game = gameObj;
+	Camera() {
+//		game = gameObj;
 		heading = 0;
-		positionX = 0;
-		positionY = 0;
+		position = new Point(10,12);
+	}
+	
+//	public Point screenCoords(Point absoluteCoords) {
+//		
+//	}
+	
+	public Point getPos() {
+		return position;
+	}
+	
+	public static void main(String[] args) {
+		int cameraX = -8;
+		int cameraY = 5;
+		double heading = -Math.PI/4;
+		
+		int pointX = -6;
+		int pointY = 3;
+		
+		AffineTransform t = new AffineTransform();
+		t.translate(-cameraX, -cameraY);
+		t.rotate(heading,cameraX,cameraY);
+		System.out.println(t.transform(new Point(pointX, pointY), new Point.Double()));
+		
+
 	}
 	
 }
