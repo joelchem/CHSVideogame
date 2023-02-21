4a9e35d4ba26c51782be545d6b5bae0a07103f45
Alexander <alexanderrauch27@gmail.com>
2023-02-21 13:46:46 -0500

    pls work

--- CHSVideogame/src/CheeseCracker.java
+++ CHSVideogame/src/CheeseCracker.java
@@ -1,7 +1,12 @@
 import java.awt.Image;
 
 public class CheeseCracker extends DisplayObject {
+<<<<<<< Updated upstream
 	public CheeseCracker(int x, int y, Game game, Image theImage) {
+=======
+	Image cheeseCrackerSprite;
+	public CheeseCracker(int x, int y, Game game, Image image) {
+>>>>>>> Stashed changes
 		super(x,y,game);
 		setSprite(theImage);
 	}
--- CHSVideogame/src/Test.java
+++ CHSVideogame/src/Test.java
@@ -1,7 +1,12 @@
 public class Test {
 
 	public static void main(String[] args) {
+<<<<<<< Updated upstream
 		System.out.println("Hello world");
 		System.out.println("3497639");
+=======
+		System.out.println("3497639");
+		
+>>>>>>> Stashed changes
 	}
 }
