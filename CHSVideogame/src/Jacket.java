1285ba2a12bfe1188c5ee88d70cba14fc5d1fdd3
MichaelMorton1 <ipibym@gmail.com>
2023-02-23 11:38:31 -0500

    Fixed constructor for Jacket

--- CHSVideogame/src/Jacket.java
+++ CHSVideogame/src/Jacket.java
@@ -1,12 +1,10 @@
-<<<<<<< HEAD
+
 import java.awt.Image;
 
-=======
->>>>>>> 8a7cc077f0cadb7e74d134ec332783327d79b3f1
 public class Jacket extends DisplayObject{
 	Game game;
-	public Jacket(int x, int y, Game game) {
-		super(game,y,x);
+	public Jacket(int x, int y, Game game, Image sprite) {
+		super(game,y,x, sprite);
 		this.game = game;
 	}
 
