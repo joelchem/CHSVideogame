5a06692db9ed65183a41d79ab6bde44fd58621c7
MichaelMorton1 <ipibym@gmail.com>
2023-02-23 11:36:45 -0500

    Fixed constructor for dollar

--- CHSVideogame/src/Dollar.java
+++ CHSVideogame/src/Dollar.java
@@ -1,8 +1,9 @@
+import java.awt.Image;
+
 public class Dollar extends DisplayObject{
-	
 	Game theGame;
-	public Dollar(int dimensionX, int dimensionY, Game game) {
-		super(game,dimensionX,dimensionY);
+	public Dollar(int dimensionX, int dimensionY, Game game, Image sprite) {
+		super(game,dimensionX,dimensionY, sprite);
 		theGame = game;
 	}
 	
