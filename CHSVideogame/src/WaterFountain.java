83640ae8717968c968ec9dd0e29f964941c841a6
MichaelMorton1 <ipibym@gmail.com>
2023-02-23 09:58:50 -0500

    Added more functionality to the waterFountain

--- CHSVideogame/src/WaterFountain.java
+++ CHSVideogame/src/WaterFountain.java
@@ -3,13 +3,11 @@ import java.awt.Image;
 public class WaterFountain extends DisplayObject {
 	//To be changed
 	final static int health_restored = 5;
-	private Image fountainSprite;
 	private Game game;
 
 	WaterFountain(Game userGame, int dimensionX, int dimensionY, Image image) {
 		super(userGame, dimensionX, dimensionY);
 		game = userGame;
-		fountainSprite = image;
 	}
 	
 	public void onCollision() {
