cbc53434e09658630db29a5c0a65e28bc78674d4
Bethany Cho <102619753+jellyfishyishy@users.noreply.github.com>
2023-03-19 23:46:09 -0400

    Update BakeSale.java

--- CHSVideogame/src/BakeSale.java
+++ CHSVideogame/src/BakeSale.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class BakeSale extends DisplayObject{
-	Game theGame;
+	private Game theGame;
 
 	private final int strengthRestored = 5000;
 	
